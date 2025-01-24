7277d2bf56097e681fdab3d6d8c0059999970189
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-01-25 02:40:37 +0530

    first commit

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 
-const app = express();
+export const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
