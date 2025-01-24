9ce7b15fbaae7caa306b618a70ebc26478af3e8c
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-01-25 03:01:41 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
+app.options("*", cors());
 // Connect to MongoDB
 mongoose
     .connect(process.env.MONGO_URI)
