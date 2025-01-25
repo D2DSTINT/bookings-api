607bdcf1558caf1f2ff913f2c6c24e4bd70ab351
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-01-25 13:55:11 +0530

    add:cors conf

---
+++ cors.config.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const allowedOrigins =
+    process.env.NODE_ENV === "production"
+        ? [process.env.ORIGIN_URL, process.env.ORIGIN_IP]
+        : [
+              `http://localhost:${process.env.PORT}`,
+              `http://127.0.0.1:${process.env.PORT}`,
+          ];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        // Allow requests with no origin only if in development
+        if (!origin && process.env.NODE_ENV !== "production") {
+            return callback(null, true);
+        }
+
+        // Check if the origin is in the allowed list
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS Error: Origin not allowed"));
+        }
+    },
+};
+
+export default corsOptions;
--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import express from "express";
 import mongoose from "mongoose";
 import Booking from "./models/booking.js"; // Import the Booking model
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import cors from "cors";
+import corsOptions from "./cors.config.js";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.options("*", cors());
 // Connect to MongoDB
